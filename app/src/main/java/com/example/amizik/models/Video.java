94ca1ae9d141c0c359d71bcd05189aa0c320830c
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2021-03-31 11:17:41 -0400

    playVideoActivity created

--- app/src/main/java/com/example/amizik/models/Video.java
+++ app/src/main/java/com/example/amizik/models/Video.java
@@ -1,4 +1,37 @@
 package com.example.amizik.models;
 
+import org.parceler.Parcel;
+
+@Parcel
 public class Video {
+
+    private String title;
+    private String url;
+    private String poster;
+
+    public Video(){}
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
 }
