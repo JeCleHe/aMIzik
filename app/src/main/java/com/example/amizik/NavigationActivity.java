eaecc701ecefe3d603baba04202800f22e3ec43e
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2021-04-06 19:32:20 -0400

    still working on youtube api

--- app/src/main/java/com/example/amizik/NavigationActivity.java
+++ app/src/main/java/com/example/amizik/NavigationActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -13,17 +14,23 @@ import com.example.amizik.fragments.ProfileFragment;
 import com.example.amizik.fragments.VideoFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class NavigationActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar) Toolbar toolbar;
+    @BindView(R.id.viewPager) ViewPager viewPager;
+    @BindView(R.id.bottom_navigation) BottomNavigationView bottom_navigation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
 
-        // Find the toolbar view inside the activity layout
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        // Sets the Toolbar to act as the ActionBar for this Activity window.
-        // Make sure the toolbar exists in the activity and is not null
+        ButterKnife.bind(this);
+
         setSupportActionBar(toolbar);
         //to change the toolbar's color
         toolbar.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
@@ -31,13 +38,6 @@ public class NavigationActivity extends AppCompatActivity {
         BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
         bottomNavigation.setOnNavigationItemSelectedListener(navigationListener);
 
-        if(OptionActivity.videoIsClicked == true){
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                    new VideoFragment()).commit();
-        }else if(OptionActivity.audioIsclicked == true){
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                    new AudioFragment()).commit();
-        }
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navigationListener =
--- app/src/main/java/com/example/amizik/fragments/BlankFragment.java
+++ app/src/main/java/com/example/amizik/fragments/BlankFragment.java
@@ -13,29 +13,14 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.amizik.R;
-import com.google.android.exoplayer2.ExoPlayerFactory;
-import com.google.android.exoplayer2.SimpleExoPlayer;
-import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
-import com.google.android.exoplayer2.extractor.ExtractorsFactory;
-import com.google.android.exoplayer2.source.ExtractorMediaSource;
-import com.google.android.exoplayer2.source.MediaSource;
-import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
-import com.google.android.exoplayer2.trackselection.TrackSelection;
-import com.google.android.exoplayer2.trackselection.TrackSelector;
-import com.google.android.exoplayer2.ui.PlayerView;
-import com.google.android.exoplayer2.upstream.BandwidthMeter;
-import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
-import com.google.android.exoplayer2.util.Util;
+
 
  public class BlankFragment extends Fragment {
 
     String title, url;
 
     TextView video_title;
-    SimpleExoPlayer player;
-   private PlayerView mExoplayerView;
+
 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
@@ -75,41 +60,9 @@ import com.google.android.exoplayer2.util.Util;
              View view = inflater.inflate(R.layout.fragment_blank, container, false);
 
              video_title = view.findViewById(R.id.video_title);
-             mExoplayerView = view.findViewById(R.id.exoplayer_view);
+
 
              video_title.setText(title);
-
-             try{
-                 //ok
-                 BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
-                 ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
-
-                 TrackSelection.Factory trackSelectionFactory = new AdaptiveTrackSelection.Factory(bandwidthMeter);
-
-                 //ok
-                 TrackSelector trackSelector = new DefaultTrackSelector(trackSelectionFactory);
-
-                 DefaultBandwidthMeter defaultBandwidthMeter = new DefaultBandwidthMeter();
-
-
-
-               DefaultDataSourceFactory dataSourceFactory = new DefaultDataSourceFactory(getContext(),
-                         Util.getUserAgent(getContext(), "video"),defaultBandwidthMeter);
-
-
-                 MediaSource mediaSource = new ExtractorMediaSource(Uri.parse("https://www.youtube.com/watch?v=Lk2ys5svDyc&t=1625s"),
-                         dataSourceFactory,
-                         extractorsFactory,
-                         null,
-                         null);
-
-                 player = (SimpleExoPlayer) ExoPlayerFactory.newSimpleInstance(getActivity(), trackSelector);
-                 mExoplayerView.setPlayer(player);
-                 player.prepare(mediaSource);
-                 player.setPlayWhenReady(true);
-             } catch (Exception e){
-                 Log.e("BlankFragment", "exoplayer error: " +  e.toString());
-             }
 
              return view;
     }
